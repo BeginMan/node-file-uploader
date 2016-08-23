1634daab0fc7570376f8ba19b36858570aaac552
beginman <xinxinyu2011@163.com>
2016-08-23 18:10:49 +0800

    说明

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -14,18 +14,21 @@ $('#upload-input').on('change', function(){
         var formData = new FormData();
 
         // 遍历所有选定的文件,并将它们添加到formData对象
+        // <input type="file">能上传多个文件，只需添加multiple或multiple="multiple"属性
         for(var i=0; i < files.length; i++) {
             var file = files[i];
             formData.append('uploads[]', file, file.name);
         }
 
+        // 关于FormData可参考:http://www.jianshu.com/p/46e6e03a0d53
         // ajax
+
         $.ajax({
             url: '/upload',
             type: 'POST',
             data: formData,
-            processData: false,
-            contentType: false,
+            processData: false,     // processData设置为false。因为data值是FormData对象，不需要对数据做处理
+            contentType: false,     // contentType设置为false。因为是由<form>表单构造的FormData对象，且已经声明了属性enctype="multipart/form-data"，所以这里设置为false。
             success: function (data) {
                 console.log('upload successful!\n' + data);
             },
